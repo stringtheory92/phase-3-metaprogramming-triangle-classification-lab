31c938651f807d3d9c167264fb8186ef4366b317
Adam Kabak <adamkabak@yahoo.com>
2022-10-31 07:28:34 -0400

    commit tree 12cdcc94a54674aa58ee7cdd0e21a21c453451cb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
+require 'awesome_print'
+
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind 
+    if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0 || @side_1 + @side_2 < @side_3 || @side_1 + @side_3 < @side_2 ||@side_2 + @side_3 < @side_1
+      raise TriangleError 
+    end
+    if @side_1 == @side_2 && @side_2 == @side_3
+      return :equalateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      return :isosceles
+    else return :scalene 
+    end
+  end
+
+  class TriangleError < StandardError 
+
+  end
 end
+
+triangle_equal = Triangle.new(1, 1, 1)
+triangle_error = Triangle.new(0, 1, 4)
+ap triangle_error.kind
